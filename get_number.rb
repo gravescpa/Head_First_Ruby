284f87069d9e41bbd7bec6cdd1cf1b8a6d6ba8a4
Susan Graves <susangraves@Susans-MacBook-Air.local>
2016-08-26 17:52:46 -0400

    completed

--- get_number.rb
+++ get_number.rb
@@ -11,13 +11,40 @@ name = input.chomp
 puts "Welcome, #{name}!"
 
 #Store a random number for the player to guess.
-
 puts "I've got a random number 1 and 100."
 puts "Can you guess it?"
 target = rand(100) + 1
 
-#Keep track of number of guesses
-
+#Keep track of number of guesses have been made
 num_guesses = 0
-remaining_guesses = 10 - num_guesses
-puts "You've got #{10 - num_guesses} guesses left."
+
+#Track whether the player has guessed correctly
+guessed_it = false
+
+until num_guesses == 10 || guessed_it
+
+	puts "You've got #{10 - num_guesses} guesses left."
+	print "Make a guess: "
+	guess = gets.to_i
+
+	num_guesses += 1
+
+	#Compare the guess to the target
+	#Print the appropriate message
+	if guess < target
+		puts "Oops, your guess was LOW."
+	elsif guess > target
+		puts "Oops, your guess was HIGH."
+	elsif guess == target
+		puts "Good job, #{name}!"
+		puts "You guessed my number in #{num_guesses} guesses!"
+		guessed_it = true
+	end
+
+end
+
+#If player ran out of turns, tell them what the number was.
+unless guessed_it
+	puts "Sorry.  You didn't get the number.  (It was #{target}.)"
+end
+
